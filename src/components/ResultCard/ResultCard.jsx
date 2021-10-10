1c11ff3f892975c139ab3523f57d3c8854bf2988
Chris Spohn <chris.spohn@vidmob.com>
2021-10-10 14:58:17 -0600

    Add ugly styling for results cards

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import './ResultCard.scss';
+import octocat from '../../assets/octocat.png'
+import eye from '../../assets/eye.png'
+
+import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 const ResultCard = ({repositoryDetails}) => {
-  const {name, id, language, owner, updated_at, url, watchers} = repositoryDetails;
+  const {name, id, language, owner, updated_at, watchers} = repositoryDetails;
+
+  const handleClick = () => {
+
+  }
 
   return (
-    <article className="result-card">
-      <h1>{name}</h1>
-      <p>{language}</p>
-      <p>{url}</p>
-      <p>{updated_at}</p>
-      <p>{watchers}</p>
-    </article>
+    <Link className="result-card" to={`/repository/results/${id}`} onClick={handleClick}>
+      <h1 className="repo-name">{name}</h1>
+      <div className="repo-details">
+        <img className="octocat" src={octocat}/>
+        <div className="line-item-details">
+          <div>
+            <p className="repo-owner">Owner: {owner.login}</p>
+            <p className="repo-language">{language}</p>
+          </div>
+          <div>
+            <div className="icon-container">
+              <img className="icon" src={eye}/>
+              <p className="repo-watchers">{watchers}</p>
+            </div>
+            <p className="repo-last-updated">Last updated: {moment(updated_at).format('MM-DD-YYYY')}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
 
