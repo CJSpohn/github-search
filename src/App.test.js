a96de71267a9ccc3f63f15f9fc00565163b7ddd7
Chris Spohn <chris.spohn@vidmob.com>
2021-10-11 11:30:11 -0600

    Add spec file for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import {createMemoryHistory} from 'history';
+import {Router} from 'react-router-dom';
+import {render, screen} from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The application', () => {
+  it('should render the search field on the home page', () => {
+    const history = createMemoryHistory();
+    history.push('/')
+    render(
+      <Router history={history}>
+        <App/>
+      </Router>
+    );
+    expect(screen.getByText(/GitHub Repository Search/)).toBeInTheDocument;
+  });
+
+  it('should render the details on the details page', () => {
+    const history = createMemoryHistory();
+    history.push('/repository/results/CJSpohn/Test-Repo')
+    render(
+      <Router history={history}>
+        <App/>
+      </Router>
+    );
+    expect(screen.getByText(/Back to Search/)).toBeInTheDocument;
+  });
+
+  it('should render the 404 page if no routes are found', () => {
+    const history = createMemoryHistory();
+    history.push('/fakeroute/fake')
+    render(
+      <Router history={history}>
+        <App/>
+      </Router>
+    );
+    expect(screen.getByText(/We haven't built a page at this location/)).toBeInTheDocument;
+  });
 });
