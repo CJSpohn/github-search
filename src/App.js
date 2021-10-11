80d2ab24855b8dd704a879d57b83e7c280470d10
Chris Spohn <chris.spohn@vidmob.com>
2021-10-10 21:47:19 -0600

    Add repo name to search results

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import RepoDetails from '../src/components/RepoDetails/RepoDetails';
 import './App.scss';
 
 const App = () => {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <Switch>
       <Route exact path="/">
         <div className="home-page">
           <Header/>
-          <Search setLoading={setLoading} setRepositories={setRepositories}/>
-          <SearchResults loading={loading} results={repositories}/>
+          <Search setSearchTerm={setSearchTerm} setLoading={setLoading} setRepositories={setRepositories}/>
+          <SearchResults searchTerm={searchTerm} loading={loading} results={repositories}/>
         </div>
       </Route>
       <Route exact path="/repository/results/:owner/:name">
