e79cec9a9c8d620fcb48deffca81fa96f03b3f30
Chris Spohn <chris.spohn@vidmob.com>
2021-10-10 21:47:55 -0600

    Remove unused scss file

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import './Loading.scss';
+import ContentLoader from 'react-content-loader';
+import PropTypes from 'prop-types';
 
-const Loading = () => {
+const Loading = ({details}) => {
   return (
-    <h1>
-      Loading...
-    </h1>
+    <>
+      {details ? 
+      <ContentLoader className="loader" viewBox="0 0 400 100">
+        <rect x="75" y="0" rx="2" ry="2" width="250" height="100" />
+      </ContentLoader> : 
+      <ContentLoader className="loader" viewBox="0 0 400 100">
+        <rect x="75" y="0" rx="2" ry="2" width="250" height="18" />
+        <rect x="75" y="20" rx="2" ry="2" width="250" height="18" />
+        <rect x="75" y="40" rx="2" ry="2" width="250" height="18" />
+        <rect x="75" y="60" rx="2" ry="2" width="250" height="18" />
+        <rect x="75" y="80" rx="2" ry="2" width="250" height="18" />
+      </ContentLoader>}
+    </>
   )
+}
+
+Loading.propTypes = {
+  details: PropTypes.bool
+}
+
+Loading.defaultProps = {
+  details: false
 }
 
 export default Loading;
