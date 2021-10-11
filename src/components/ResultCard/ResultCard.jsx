bf0ad1fc4c94e8996278ba5206bb55fb3f0a6be0
Chris Spohn <chris.spohn@vidmob.com>
2021-10-11 11:32:27 -0600

    Add alts to images

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import './ResultCard.scss';
-import eye from '../../assets/eye.png'
-import PropTypes from 'prop-types';
-
-import {Link} from 'react-router-dom';
 import moment from 'moment';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
+import './ResultCard.scss';
+
+import eye from '../../assets/eye.png'
 
 const ResultCard = ({repositoryDetails}) => {
-  const {name, id, language, owner, updated_at, watchers} = repositoryDetails;
+  const {name, language, owner, updated_at, watchers} = repositoryDetails;
 
   const filteredName = name.length > 25 ? name.slice(0, 25) + '...' : name;
   return (
     <Link className="result-card" to={`/repository/results/${owner.login}/${name}`}>
       <h1 className="repo-name">{filteredName}</h1>
       <div className="repo-details">
-        <img className="octocat" src={owner.avatar_url}/>
+        <img alt="the GitHub logo" className="octocat" src={owner.avatar_url}/>
         <div className="line-item-details">
           <div>
             <p className="repo-owner">Owner: {owner.login}</p>
@@ -22,7 +23,7 @@ const ResultCard = ({repositoryDetails}) => {
           </div>
           <div>
             <div className="icon-container">
-              <img className="icon" src={eye}/>
+              <img className="icon" alt="an eyeball" src={eye}/>
               <p className="repo-watchers">{watchers}</p>
             </div>
             <p className="repo-last-updated">Last updated: {moment(updated_at).format('MM-DD-YYYY')}</p>
