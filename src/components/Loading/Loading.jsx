a07cb8f04226af964504da01f81d0d40ac995a2e
Chris Spohn <chris.spohn@vidmob.com>
2021-10-11 11:31:44 -0600

    Style loader to match actual componenets

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -6,16 +6,22 @@ const Loading = ({details}) => {
   return (
     <>
       {details ? 
-      <ContentLoader className="loader" viewBox="0 0 400 100">
-        <rect x="75" y="0" rx="2" ry="2" width="250" height="100" />
-      </ContentLoader> : 
-      <ContentLoader className="loader" viewBox="0 0 400 100">
-        <rect x="75" y="0" rx="2" ry="2" width="250" height="18" />
-        <rect x="75" y="20" rx="2" ry="2" width="250" height="18" />
-        <rect x="75" y="40" rx="2" ry="2" width="250" height="18" />
-        <rect x="75" y="60" rx="2" ry="2" width="250" height="18" />
-        <rect x="75" y="80" rx="2" ry="2" width="250" height="18" />
-      </ContentLoader>}
+        <ContentLoader viewBox="0 0 400 100">
+          <rect x="75" y="0" rx="2" ry="2" width="250" height="100"/>
+        </ContentLoader> : 
+        <>
+          <ContentLoader viewbox="0 0 100 100" width="200" height="75">
+            <rect x="0" y="0" rx="2" ry="2" width="200" height="50"/>
+          </ContentLoader>
+          <ContentLoader viewBox="0 0 400 100" style={{width: '1000px', height: '250px'}}>
+            <rect x="75" y="0" rx="2" ry="2" width="250" height="18"/>
+            <rect x="75" y="20" rx="2" ry="2" width="250" height="18"/>
+            <rect x="75" y="40" rx="2" ry="2" width="250" height="18"/>
+            <rect x="75" y="60" rx="2" ry="2" width="250" height="18"/>
+            <rect x="75" y="80" rx="2" ry="2" width="250" height="18"/>
+          </ContentLoader>
+        </>
+      }
     </>
   )
 }
